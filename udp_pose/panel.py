3e3644be46708a4e3348d8194d0e9f8bd2428a26
Lukas Erlacher <l.erlacher@gmail.com>
2015-06-22 10:23:05 +0200

    simplify locking

--- udp_pose/panel.py
+++ udp_pose/panel.py
@@ -5,38 +5,31 @@ class Switch:
     self.positions = positions
     self.label = label
     self.idx = defidx
-    self.master = None
     self.slaves = []
     self.locked = False
 
-  def set(self, idx, chain):
+  def set(self, idx, chain=True):
     if chain == None:
       chain = True
     if idx >= 0 and idx < len(self.positions):
       self.idx = idx
       for slave in self.slaves:
         if slave.locked and chain:
-          slave.set(idx, True)
-      if self.master and self.master.locked:
-        self.master.set(idx, False)
+          slave.set(idx, False)
 
-  def inc(self, chain):
+  def inc(self, chain=True):
     if self.idx < len(self.positions)-1:
       self.idx += 1
       for slave in self.slaves:
         if slave.locked and chain:
-          slave.inc(True)
-      if self.master and self.master.locked and chain:
-        self.master.inc(False)
+          slave.inc(False)
 
-  def dec(self, chain):
+  def dec(self, chain=True):
     if self.idx > 0:
       self.idx -= 1
       for slave in self.slaves:
         if slave.locked and chain:
-          slave.dec(True)
-      if self.master and self.master.locked and chain:
-        self.master.dec(False)
+          slave.dec(False)
 
   def lock(self):
     self.locked = True
@@ -45,6 +38,8 @@ class Switch:
 
   def unlock(self):
     self.locked = False
+    for slave in self.slaves:
+      slave.locked = False
 
   def toggle_lock(self):
     if self.locked:
@@ -68,7 +63,11 @@ class Panel:
     for swidx in range(len(self.switches)):
       switch = self.switches[swidx]
       if swidx == self.switch_idx:
-        screen.addstr(line,0,'>')
+        if switch.locked:
+          screen.addstr(line,0,'>>')
+        else:
+          screen.addstr(line,0,'>')
+
 
       screen.addstr(line, 2, switch.label)
       for idx in range(len(switch.positions)):
@@ -87,11 +86,12 @@ def main(stdscr):
   lthrottle = Switch("L", [0,1,2,3,4,5,6,7,8,9,10,11,12],0)
   rthrottle = Switch("R", [0,1,2,3,4,5,6,7,8,9,10,11,12],0)
   lthrottle.slaves = [rthrottle]
-  rthrottle.master = lthrottle
+  rthrottle.slaves = [lthrottle]
   rthrottle.lock()
+  lthrottle.lock()
 
   p.switches.append(lthrottle)
-  p.switches.append(lthrottle)
+  p.switches.append(rthrottle)
   p.switches.append(Switch("Gear", ['UP','DN'],1))
 
   p.render(stdscr)
